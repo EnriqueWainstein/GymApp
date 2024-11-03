ad9487e156ef0cb40f8c03b235602a22e09746b0
Ivan Agustin Zarate <ivan.agustin.95@gmail.com>
2024-11-03 19:25:31 -0300

    Listio las migras

--- GymApp.MVC/GymApp.MVC/Contexto/GymContext.cs
+++ GymApp.MVC/GymApp.MVC/Contexto/GymContext.cs
@@ -8,12 +8,39 @@ namespace GymApp.MVC.Data
         public GymContext(DbContextOptions<GymContext> options) : base(options)
         {
         }
-        public DbSet<PlanRutina> planes { get; set; }
+        public DbSet<PlanRutina> Planes { get; set; }
         public DbSet<Miembro> Miembros { get; set; }
         public DbSet<Clase> Clases { get; set; }
-        
         public DbSet<Membresia> Membresias { get; set; }
         public DbSet<Entrenador> Entrenadores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Clase>()
+                .HasMany(c => c.MiembrosInscritos)
+                .WithMany(m => m.ClasesInscritas)
+                .UsingEntity(j => j.ToTable("ClaseMiembro"));
+
+            modelBuilder.Entity<Clase>()
+                .HasOne(c => c.Entrenador)
+                .WithMany(e => e.ClasesAsignadas)
+                .HasForeignKey(c => c.EntrenadorId);
+
+            modelBuilder.Entity<Membresia>()
+                .HasOne(m => m.Miembro)
+                .WithOne(mb => mb.MembresiaActual)
+                .HasForeignKey<Membresia>(m => m.MiembroId);
+
+            modelBuilder.Entity<PlanRutina>()
+                .HasOne(pr => pr.Miembro)
+                .WithMany()
+                .HasForeignKey(pr => pr.MiembroId);
+
+            modelBuilder.Entity<PlanRutina>()
+                .HasOne(pr => pr.Entrenador)
+                .WithMany()
+                .HasForeignKey(pr => pr.EntrenadorId);
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ GymApp.MVC/GymApp.MVC/Migrations/20241103222314_InitialCreate.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymApp.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entrenadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Especialidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entrenadores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Miembros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Miembros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Horario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EntrenadorId = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clases_Entrenadores_EntrenadorId",
+                        column: x => x.EntrenadorId,
+                        principalTable: "Entrenadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membresias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MiembroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membresias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Membresias_Miembros_MiembroId",
+                        column: x => x.MiembroId,
+                        principalTable: "Miembros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MiembroId = table.Column<int>(type: "int", nullable: false),
+                    EntrenadorId = table.Column<int>(type: "int", nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ejercicios = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Planes_Entrenadores_EntrenadorId",
+                        column: x => x.EntrenadorId,
+                        principalTable: "Entrenadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Planes_Miembros_MiembroId",
+                        column: x => x.MiembroId,
+                        principalTable: "Miembros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClaseMiembro",
+                columns: table => new
+                {
+                    ClasesInscritasId = table.Column<int>(type: "int", nullable: false),
+                    MiembrosInscritosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClaseMiembro", x => new { x.ClasesInscritasId, x.MiembrosInscritosId });
+                    table.ForeignKey(
+                        name: "FK_ClaseMiembro_Clases_ClasesInscritasId",
+                        column: x => x.ClasesInscritasId,
+                        principalTable: "Clases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClaseMiembro_Miembros_MiembrosInscritosId",
+                        column: x => x.MiembrosInscritosId,
+                        principalTable: "Miembros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClaseMiembro_MiembrosInscritosId",
+                table: "ClaseMiembro",
+                column: "MiembrosInscritosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clases_EntrenadorId",
+                table: "Clases",
+                column: "EntrenadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membresias_MiembroId",
+                table: "Membresias",
+                column: "MiembroId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planes_EntrenadorId",
+                table: "Planes",
+                column: "EntrenadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planes_MiembroId",
+                table: "Planes",
+                column: "MiembroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClaseMiembro");
+
+            migrationBuilder.DropTable(
+                name: "Membresias");
+
+            migrationBuilder.DropTable(
+                name: "Planes");
+
+            migrationBuilder.DropTable(
+                name: "Clases");
+
+            migrationBuilder.DropTable(
+                name: "Miembros");
+
+            migrationBuilder.DropTable(
+                name: "Entrenadores");
+        }
+    }
+}
